1dabb910250512b1b86f233ca6980b4e82aef48d
Dale Black <djblack@uci.edu>
2026-03-06 04:27:38 -0800

    TIMEOUT-SAVE: Auto-commit dirty state from sessions-v3 iteration 1

--- test/fixtures/basic_notebook.jl
+++ test/fixtures/basic_notebook.jl
@@ -10,7 +10,58 @@ y = x + 1
 # ╔═╡ 00000003-0000-0000-0000-000000000003
 z = x * y
 
+# ╔═╡ d0af9a99-dfe9-4f7a-b2a2-2a72241e77d7
+begin
+    sleep(5)
+    a = 1
+end
+
+# ╔═╡ d8f99fd1-c355-4ea8-b049-fe02fef97966
+begin
+    sleep(4)
+    b = a * 2
+end
+
+# ╔═╡ bc4437d0-89f5-4862-8c07-32cf149cb296
+begin
+    sleep(5)
+    c = b * 2
+end
+
+# ╔═╡ a6194bff-a34c-4e43-b2d7-e62997ccbf34
+using Markdown
+
+# ╔═╡ 1013125c-cf52-4963-8d34-1afacd91f05b
+md"""
+## This is an H2
+
+* This should be a bullet
+
+1. This should be **BOLD**
+
+"""
+
+# ╔═╡ b236809c-50e5-45e4-8158-f41bad0e9103
+
+# ╔═╡ bc9c0418-fe44-47a5-a7fa-c6c26c776d3c
+
+# ╔═╡ 2963dc7d-2ee9-48e6-a93e-3eb1457bec86
+
+# ╔═╡ 0ab3d0c8-b8c7-4723-8d53-e50d221ec3a7
+
+# ╔═╡ 265a22e7-9448-4e94-a667-bcbed2268af6
+
 # ╔═╡ Cell order:
 # ╠═00000001-0000-0000-0000-000000000001
 # ╠═00000002-0000-0000-0000-000000000002
 # ╠═00000003-0000-0000-0000-000000000003
+# ╠═d0af9a99-dfe9-4f7a-b2a2-2a72241e77d7
+# ╠═d8f99fd1-c355-4ea8-b049-fe02fef97966
+# ╠═bc4437d0-89f5-4862-8c07-32cf149cb296
+# ╠═a6194bff-a34c-4e43-b2d7-e62997ccbf34
+# ╠═1013125c-cf52-4963-8d34-1afacd91f05b
+# ╠═b236809c-50e5-45e4-8158-f41bad0e9103
+# ╠═bc9c0418-fe44-47a5-a7fa-c6c26c776d3c
+# ╠═2963dc7d-2ee9-48e6-a93e-3eb1457bec86
+# ╠═0ab3d0c8-b8c7-4723-8d53-e50d221ec3a7
+# ╠═265a22e7-9448-4e94-a667-bcbed2268af6
